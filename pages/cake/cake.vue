89ced93d577807e2298696ef67b209cfbd47c101
kayi <747735562@qq.com>
2022-10-27 22:47:08 +0800

    uni.navigateTo uni.navigateBack uni.switchTab用法（跳转页面）

--- pages/cake/cake.vue
+++ pages/cake/cake.vue
@@ -1,9 +1,9 @@
 <template>
 	<view>
 		<view class="cont">
-			<view class="cake-item" v-for="(item,index) in 8">
+			<view class="cake-item" v-for="(item,index) in 8" @click="handleDetail(index)">
 				<view class="poster">
-			
+
 				</view>
 				<view class="info-cont">
 					<view class="info">
@@ -19,7 +19,7 @@
 						</view>
 					</view>
 					<view class="cart-btn">
-			
+
 					</view>
 				</view>
 			</view>
@@ -32,22 +32,28 @@
 		data() {
 			return {
 
-			}
+			} 
 		},
 		methods: {
-
+		handleDetail(idx){
+			console.log(idx);
+			uni.navigateTo({
+				url:'/pages/detail/detail?idx='+idx
+			})
 		}
 	}
+}
 </script>
 
 <style lang="scss">
-	.cont{
+	.cont {
 		display: flex;
 		flex-wrap: wrap;
 		padding: 15upx;
 		justify-content: space-between;
-		
+
 	}
+
 	.cake-item {
 		width: 350upx;
 
@@ -55,13 +61,16 @@
 			height: 350upx;
 			background-color: #f5f5f5;
 		}
-		.fs-28{
+
+		.fs-28 {
 			margin-top: 24upx;
 		}
-		.fs-16{
+
+		.fs-16 {
 			margin: 14upx 0;
 		}
-		.fs-18{
+
+		.fs-18 {
 			margin-bottom: 22upx;
 		}
 	}
---
+++ pages/detail/detail.vue
@@ -0,0 +1,43 @@
+<template>
+	<view>
+		<button type="primary" @click="handleBack">
+			返回
+		</button>
+		<button type="default" @click="handleMine">
+			访问个人中心
+		</button>
+	商品详情
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad(options){
+			console.log(options);
+		},
+		methods:{
+			handleBack(){
+				uni.navigateBack({
+					delta:1
+				})
+			},
+			handleMine(){
+				uni.switchTab({
+					url:'/pages/food/food'
+				})
+				// uni.navigateTo({
+				// 	url:'/pages/my/my'
+				// }) 可跳转但不可以跳转底部菜单页（my现在不是底部菜单页）
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
